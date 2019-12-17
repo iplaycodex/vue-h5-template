d33a2e3841918428b30c235ec3a607c208c7f990
liuliang <liuliang@111.com.cn>
2019-12-17 17:25:18 +0800

    autoprefixer 的配置写在 package.json 中

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -5,9 +5,6 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    'autoprefixer': {
-      browsers: ['Android >= 4.0', 'iOS >= 7']
-    },
     'postcss-pxtorem': {
       rootValue: 37.5,  //375
       propList: ['*']
